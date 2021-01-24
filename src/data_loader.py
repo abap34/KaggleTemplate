572e51e93138c80749e3a9106f1179ad5b42e778
abap34 <uchi_ymgc@her0.jp>
2021-01-24 14:59:44 +0900

    Update data_loder.py; tqdm表示を追加

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,20 +1,22 @@
 import glob
 import pandas as pd
 import os
+from tqdm import tqdm
 
 
 def _load_data(full_path):
+    print('read:', full_path)
     files = glob.glob(full_path)
     df = pd.DataFrame()
-    for file in files:
+    for file in tqdm(files):
         fe = pd.read_feather(file)
         df = pd.concat([df, fe], axis=1)
     df.sort_index(axis=1, inplace=True)
     return df
 
 def load_data(file_dir):
-    train = _load_data(os.path.join(file_dir, 'train'))
-    test = _load_data(os.path.join(file_dir, 'test'))
+    train = _load_data(os.path.join(file_dir, 'train/*.feather'))
+    test = _load_data(os.path.join(file_dir, 'test/*.feather'))
     
     return train, test
 
