10da4d790277b49f30493df19ee39e7e0cc9f557
abap34 <uchi_ymgc@her0.jp>
2021-01-24 20:47:47 +0900

    Update run.py; クオーテーションを統一

--- src/NNModels.py
+++ src/NNModels.py
@@ -5,12 +5,15 @@ import wandb
 import pandas as pd
 import numpy as np
 
+
 class SimpleMLPRegressor(base.BaseModel):
-    def __init__(self, config : Dict) -> None:
+    def __init__(self, config: Dict) -> None:
         model = tf.keras.Sequential()
         model.add(tf.keras.layers.Dropout(config['model']['input_drop_rate']))
         for unit in config['model']['units']:
-            model.add(tf.keras.layers.Dense(unit, activation=config['model']['activation']))
+            model.add(
+                tf.keras.layers.Dense(unit, activation=config['model']['activation'])
+            )
             if config['model']['batch_norm']:
                 model.add(tf.keras.layers.BatchNormalization())
             model.add(tf.keras.layers.Dropout(config['model']['drop_rate']))
@@ -18,31 +21,34 @@ class SimpleMLPRegressor(base.BaseModel):
         model.add(tf.keras.layers.Dense(1))
 
         optimizer_type = config['optimizer']['optimizer']
-        optimizer_arg = {i:config['optimizer'][i] for i in config['optimizer'] if i != 'optimizer'}
+        optimizer_arg = {
+            i: config['optimizer'][i] for i in config['optimizer'] if i != 'optimizer'
+        }
 
-        if optimizer_type == "sgd":
-            optimizer = tf.keras.optimizers.SGD(
-                **optimizer_arg
-            )
-        elif optimizer_type == "adam":
-            optimizer = tf.keras.optimizers.Adam(
-                **optimizer_arg
-            )
+        if optimizer_type == 'sgd':
+            optimizer = tf.keras.optimizers.SGD(**optimizer_arg)
+        elif optimizer_type == 'adam':
+            optimizer = tf.keras.optimizers.Adam(**optimizer_arg)
         else:
-            err_msg = "ignore optimizer. passed {}".format(optimizer_type)
+            err_msg = 'ignore optimizer. passed {}'.format(optimizer_type)
             raise ValueError(err_msg)
 
-        
-        model.compile(optimizer, loss=config['model']['loss'], metrics=config['model']['metrics'])
+        model.compile(
+            optimizer, loss=config['model']['loss'], metrics=config['model']['metrics']
+        )
 
         self.model = model
 
         self.config = config
 
-    def _fit(self, train_x : pd.DataFrame , train_y : pd.DataFrame, val_x : pd.DataFrame, val_y : pd.DataFrame) -> None: 
-        reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
-            **self.config['reduce_lr']
-        )
+    def _fit(
+        self,
+        train_x: pd.DataFrame,
+        train_y: pd.DataFrame,
+        val_x: pd.DataFrame,
+        val_y: pd.DataFrame,
+    ) -> None:
+        reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(**self.config['reduce_lr'])
         early_stopping = tf.keras.callbacks.EarlyStopping(
             **self.config['early_stopping']
         )
@@ -53,50 +59,58 @@ class SimpleMLPRegressor(base.BaseModel):
             validation_data=(val_x.values, val_y.values),
             batch_size=self.config['fit']['batch_size'],
             verbose=self.config['fit']['verbose'],
-            callbacks=[reduce_lr, early_stopping,  wandb.keras.WandbCallback()],
+            callbacks=[reduce_lr, early_stopping, wandb.keras.WandbCallback()],
         )
 
-    def _predict(self, test_x : pd.DataFrame) -> np.ndarray:
+    def _predict(self, test_x: pd.DataFrame) -> np.ndarray:
         return self.model.predict(test_x.values)
 
 
 class SimpleMLPClassifier(base.BaseModel):
-    def __init__(self, config : Dict) -> None:
+    def __init__(self, config: Dict) -> None:
         model = tf.keras.Sequential()
         model.add(tf.keras.layers.Dropout(config['model']['input_drop_rate']))
         for unit in config['model']['units']:
-            model.add(tf.keras.layers.Dense(unit, activation=config['model']['activation']))
+            model.add(
+                tf.keras.layers.Dense(unit, activation=config['model']['activation'])
+            )
             if config['model']['batch_norm']:
                 model.add(tf.keras.layers.BatchNormalization())
             model.add(tf.keras.layers.Dropout(config['model']['drop_rate']))
 
-        model.add(tf.keras.layers.Dense(config['model']['n_class'], activation='softmax'))
+        model.add(
+            tf.keras.layers.Dense(config['model']['n_class'], activation='softmax')
+        )
 
         optimizer_type = config['optimizer']['optimizer']
-        optimizer_arg = {i:config['optimizer'][i] for i in config['optimizer'] if i != 'optimizer'}
+        optimizer_arg = {
+            i: config['optimizer'][i] for i in config['optimizer'] if i != 'optimizer'
+        }
 
-        if optimizer_type == "sgd":
-            optimizer = tf.keras.optimizers.SGD(
-                **optimizer_arg
-            )
-        elif optimizer_type == "adam":
-            optimizer = tf.keras.optimizers.Adam(
-                **optimizer_arg
-            )
+        if optimizer_type == 'sgd':
+            optimizer = tf.keras.optimizers.SGD(**optimizer_arg)
+        elif optimizer_type == 'adam':
+            optimizer = tf.keras.optimizers.Adam(**optimizer_arg)
         else:
-            err_msg = "ignore optimizer. passed {}".format(optimizer_type)
+            err_msg = 'ignore optimizer. passed {}'.format(optimizer_type)
             raise ValueError(err_msg)
 
-        model.compile(optimizer, loss=config['model']['loss'], metrics=config['model']['metrics'])
+        model.compile(
+            optimizer, loss=config['model']['loss'], metrics=config['model']['metrics']
+        )
 
         self.model = model
 
         self.config = config
 
-    def _fit(self, train_x : pd.DataFrame , train_y : pd.DataFrame, val_x : pd.DataFrame, val_y : pd.DataFrame) -> None: 
-        reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
-            **self.config['reduce_lr']
-        )
+    def _fit(
+        self,
+        train_x: pd.DataFrame,
+        train_y: pd.DataFrame,
+        val_x: pd.DataFrame,
+        val_y: pd.DataFrame,
+    ) -> None:
+        reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(**self.config['reduce_lr'])
         early_stopping = tf.keras.callbacks.EarlyStopping(
             **self.config['early_stopping']
         )
@@ -107,8 +121,8 @@ class SimpleMLPClassifier(base.BaseModel):
             validation_data=(val_x.values, val_y.values),
             batch_size=self.config['fit']['batch_size'],
             verbose=self.config['fit']['verbose'],
-            callbacks=[reduce_lr, early_stopping,  wandb.keras.WandbCallback()],
+            callbacks=[reduce_lr, early_stopping, wandb.keras.WandbCallback()],
         )
-    
-    def _predict(self, test_x : pd.DataFrame) -> np.ndarray:
+
+    def _predict(self, test_x: pd.DataFrame) -> np.ndarray:
         return self.model.predict(test_x.values)
